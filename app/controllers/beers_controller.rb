5ce058e1c11beada0c01d11dca0ed63e52e25eeb
James Hughes <james.a.hughes@gmail.com>
2017-04-04 21:21:08 -0500

    Add index

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,9 @@
 class BeersController < ApplicationController
+  def index
+    @beers = Unirest.get("http://localhost:3000/api/v2/beers").body
+    render "index.html.erb"
+  end
+
   def show
     @beer = Unirest.get("http://localhost:3000/api/v2/beers/#{params[:id]}").body
     render "show.html.erb"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "/beers" => "beers#index"
   get "/beers/:id" => "beers#show"
 end
